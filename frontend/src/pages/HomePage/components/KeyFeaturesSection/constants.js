17d1225b15d54f89517f184bcd0985c52b4d96f0
sofiaprg <sofiaskymail@gmail.com>
2024-11-12 11:46:16 +0200

    Updated with flask and react

---
+++ frontend/src/pages/HomePage/components/KeyFeaturesSection/constants.js
@@ -0,0 +1,32 @@
+import ClockIcon from "../../../../assets/icons/clock.png";
+import CalendarIcon from "../../../../assets/icons/calendar.png";
+import ChartIcon from "../../../../assets/icons/chart.png";
+import TickIcon from "../../../../assets/icons/tick.png";
+
+export const cards = [
+  {
+    icon: ClockIcon,
+    title: "Real time",
+    description:
+      "Our model is updated daily, providing you with the most recent predictions.",
+  },
+
+  {
+    icon: CalendarIcon,
+    title: "High Frequency",
+    description:
+      "Our model forecasts the unemployment rate for the next 12 months, enabling you to plan ahead.",
+  },
+
+  {
+    icon: ChartIcon,
+    title: "Accurate",
+    description: "Our model is accurate and outperforms most other forecasts.",
+  },
+
+  {
+    icon: TickIcon,
+    title: "Easy to use",
+    description: "Our model is easy to use. Sign up to access our API.",
+  },
+];
---
+++ frontend/src/pages/HomePage/components/UnemploymentRateMatterSection/constants.js
@@ -0,0 +1,34 @@
+import BarChartImage from "../../../../assets/bar_chart.png";
+import MeetingImage from "../../../../assets/meeting.png";
+import IncreaseImage from "../../../../assets/increase.png";
+import LineChartImage from "../../../../assets/line_chart.png";
+
+export const cards = [
+  {
+    image: BarChartImage,
+    title: "Unemployment rate (U-3)",
+    description:
+      "The most commonly cited unemployment rate, U-3 includes people who are out of work and actively seeking employment",
+  },
+
+  {
+    image: MeetingImage,
+    title: "Labor force participation",
+    description:
+      "The percentage of working-age Americans who are either employed or unemployed and actively seeking work",
+  },
+
+  {
+    image: IncreaseImage,
+    title: "U-6 underemployment rate",
+    description:
+      "A broader measure of joblessness, U-6 includes not only the unemployed but also part-time workers who want full-time jobs and discouraged workers who have stopped looking for work.",
+  },
+
+  {
+    image: LineChartImage,
+    title: "Long-term unemployment rate ",
+    description:
+      "The percentage of the labor force that has been unemployed for 27 weeks or longer.",
+  },
+];
