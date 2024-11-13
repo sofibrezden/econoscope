7b75e6d72421125325ee7e267d96fa18fba86a97
sofibrezden <sofiaskymail@gmail.com>
2024-11-13 21:54:26 +0200

    add history page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,8 @@ import React from "react";
 import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Visualization from "./pages/HomePage/components/Visualization";
-import UserHistory from "./pages/HomePage/components/UserHistory";
 import Login from "./pages/HomePage/Login";
+import UserHistoryPage from "./pages/UserHistoryPage/UserHistoryPage";
 function App() {
   return (
     <Router>
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/predict" element={<PredictorForm />} />
         <Route path="/visualization" element={<Visualization />} />
-        <Route path="/history" element={<UserHistory />} />
+        <Route path="/history" element={<UserHistoryPage />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
