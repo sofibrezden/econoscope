e4a3233a91982b92bfa5f42e660e1a18669d594c
sofibrezden <sofiaskymail@gmail.com>
2024-11-20 22:29:51 +0200

    Added logic with JWT

--- frontend/src/pages/PredictorForm/components/UnemploymentRatePredictor.js
+++ frontend/src/pages/PredictorForm/components/UnemploymentRatePredictor.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import axios from "axios";
 import "./UnemploymentRatePredictor.scss";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+import Header from "../../HomePage/components/Header/Header";
 
 const UnemploymentRatePredictor = () => {
     const [age, setAge] = useState("");
@@ -80,54 +81,58 @@ const UnemploymentRatePredictor = () => {
     };
 
     return (
-        <div className="predictor-container">
-            <h1>Unemployment Rate Predictor</h1>
-            <div className="prediction-form">
-                <h2>Forecasting Prediction Form</h2>
+        <><Header/>
 
-                <select value={age} onChange={(e) => setAge(e.target.value)}>
-                    <option value="">Select Age</option>
-                    {ages.map((ageOption, index) => (
-                        <option key={index} value={ageOption}>
-                            {ageOption}
-                        </option>
-                    ))}
-                </select>
 
-                <select value={gender} onChange={(e) => setGender(e.target.value)}>
-                    <option value="">Select Gender</option>
-                    {genders.map((genderOption, index) => (
-                        <option key={index} value={genderOption}>
-                            {genderOption}
-                        </option>
-                    ))}
-                </select>
+            <div className="predictor-container">
+                <h1>Unemployment Rate Predictor</h1>
+                <div className="prediction-form">
+                    <h2>Forecasting Prediction Form</h2>
 
-                <select value={country} onChange={(e) => setCountry(e.target.value)}>
-                    <option value="">Select Country</option>
-                    {countries.map((countryOption, index) => (
-                        <option key={index} value={countryOption}>
-                            {countryOption}
-                        </option>
-                    ))}
-                </select>
+                    <select value={age} onChange={(e) => setAge(e.target.value)}>
+                        <option value="">Select Age</option>
+                        {ages.map((ageOption, index) => (
+                            <option key={index} value={ageOption}>
+                                {ageOption}
+                            </option>
+                        ))}
+                    </select>
 
-                <input
-                    type="number"
-                    value={year}
-                    onChange={(e) => setYear(e.target.value)}
-                    placeholder="Select Year"
-                />
+                    <select value={gender} onChange={(e) => setGender(e.target.value)}>
+                        <option value="">Select Gender</option>
+                        {genders.map((genderOption, index) => (
+                            <option key={index} value={genderOption}>
+                                {genderOption}
+                            </option>
+                        ))}
+                    </select>
 
-                <button onClick={handlePreparePrediction}>Predict</button>
+                    <select value={country} onChange={(e) => setCountry(e.target.value)}>
+                        <option value="">Select Country</option>
+                        {countries.map((countryOption, index) => (
+                            <option key={index} value={countryOption}>
+                                {countryOption}
+                            </option>
+                        ))}
+                    </select>
 
-                {prediction && (
-                    <div className="prediction-result">
-                        <p><strong>Forecasted Value:</strong> {prediction.prediction * 100}</p>
-                    </div>
-                )}
+                    <input
+                        type="number"
+                        value={year}
+                        onChange={(e) => setYear(e.target.value)}
+                        placeholder="Select Year"
+                    />
+
+                    <button onClick={handlePreparePrediction}>Predict</button>
+
+                    {prediction && (
+                        <div className="prediction-result">
+                            <p><strong>Forecasted Value:</strong> {prediction.prediction * 100}</p>
+                        </div>
+                    )}
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
