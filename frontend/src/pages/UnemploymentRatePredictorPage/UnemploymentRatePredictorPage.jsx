193156b2bc1280299207ee50535d796f00814ba9
sofiaprg <sofiaskymail@gmail.com>
2024-11-21 15:03:12 +0200

    Updated with dynamic chart

--- frontend/src/pages/UnemploymentRatePredictorPage/UnemploymentRatePredictorPage.jsx
+++ frontend/src/pages/UnemploymentRatePredictorPage/UnemploymentRatePredictorPage.jsx
@@ -1,13 +1,15 @@
 import Header from "../HomePage/components/Header/Header";
 import UnemploymentRatePredictorSection from "./components/UnemploymentRatePredictorSection";
+import Footer from "../HomePage/components/Footer/Footer";
 
 function UnemploymentRatePredictorPage() {
-  return (
-    <>
-      <Header />
-      <UnemploymentRatePredictorSection />
-    </>
-  );
+    return (
+        <>
+            <Header/>
+            <UnemploymentRatePredictorSection/>
+            <Footer/>
+        </>
+    );
 }
 
 export default UnemploymentRatePredictorPage;
