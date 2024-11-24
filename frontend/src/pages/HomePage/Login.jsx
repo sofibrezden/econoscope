ea401e934a24798e08b46d516f7d0cf1a50cfe4e
sofibrezden <sofiaskymail@gmail.com>
2024-11-24 20:24:23 +0200

    login page

--- frontend/src/pages/HomePage/Login.jsx
+++ frontend/src/pages/HomePage/Login.jsx
@@ -1,97 +1,102 @@
-import React, {useState} from 'react';
-import './Login.css';
+import React from "react";
+import "./Login.css";
 import Header from "./components/Header/Header";
+import styles from "./Login.module.scss";
+import { Field, Form, Formik } from "formik";
+import LogIn from "../../assets/login.png";
 
 function Login() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+  const handleSubmit = async (value) => {
+    try {
+      const response = await fetch("http://127.0.0.1:5000/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(value),
+      });
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error || "Login failed");
+      }
 
-        try {
-            const response = await fetch('http://127.0.0.1:5000/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({username, password}),
-            });
+      const { token } = await response.json();
+      localStorage.setItem("authToken", token);
 
-            if (!response.ok) {
-                const errorData = await response.json();
-                throw new Error(errorData.error || 'Login failed');
-            }
+      // Запит із токеном для перевірки автентифікації
+      const authCheck = await fetch("http://127.0.0.1:5000/check-auth", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`, // Додавання токена в заголовок
+        },
+      });
 
-            const {token} = await response.json();
-            localStorage.setItem('authToken', token);
+      if (!authCheck.ok) {
+        const errorData = await authCheck.json();
+        throw new Error(errorData.error || "Authentication check failed");
+      }
 
-            // Запит із токеном для перевірки автентифікації
-            const authCheck = await fetch('http://127.0.0.1:5000/check-auth', {
-                method: 'GET',
-                headers: {
-                    'Authorization': `Bearer ${token}`, // Додавання токена в заголовок
-                },
-            });
+      const authStatus = await authCheck.json();
 
-            if (!authCheck.ok) {
-                const errorData = await authCheck.json();
-                throw new Error(errorData.error || 'Authentication check failed');
-            }
+      if (authStatus.authenticated) {
+        console.log("User authenticated");
+        window.location.href = "/";
+      } else {
+        console.error("Authentication check failed.");
+      }
+    } catch (error) {
+      console.error("Login failed:", error.message);
+      alert(error.message);
+    }
+  };
 
-            const authStatus = await authCheck.json();
-
-            if (authStatus.authenticated) {
-                console.log("User authenticated");
-                window.location.href = '/';
-            } else {
-                console.error('Authentication check failed.');
-            }
-        } catch (error) {
-            console.error('Login failed:', error.message);
-            alert(error.message);
-        }
-    };
-
-    return (
-        <>
-            <Header/>
-
-            <div className="wrapper">
-                <form id="loginForm" onSubmit={handleSubmit}>
-                    <h1>Login</h1>
-                    <div className="input-box">
-                        <input
-                            type="text"
-                            id="username"
-                            placeholder="Username"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                            required
-                        />
-                        <i className="bx bx-envelope"></i>
-                    </div>
-
-                    <div className="input-box">
-                        <input
-                            type="password"
-                            id="password"
-                            placeholder="Password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                        />
-                        <i className="bx bxs-lock-alt"></i>
-                    </div>
-                    <button className="login" type="submit">Sign In</button>
-                </form>
-
-                <div className="register-link">
-                    <p>Don't have an account? <a href="/register">Register</a></p>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <div className={styles.root}>
+        <div>
+          <h1>Econoscope</h1>
+          <Formik
+            initialValues={{
+              username: "",
+              password: "",
+            }}
+            onSubmit={handleSubmit}
+          >
+            <Form className={styles.form}>
+              <p className={styles.welcome_back}>Welcome Back</p>
+              <div>
+                <label htmlFor="username">Username </label>
+                <Field id="username" name="username" placeholder="Oleg" />
+              </div>
+              <div>
+                <label htmlFor="password">Password </label>
+                <Field
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder="∗∗∗∗∗∗∗∗∗∗∗"
+                />
+              </div>
+              <div className={styles.buttons_container}>
+                <button className={styles.sign_up_button}>Sign Up</button>
+                <p className={styles.sub_text}>
+                  Don`t have an account?{" "}
+                  <a href="/register" className={styles.sign_in_link}>
+                    <b>Sign Up</b>
+                  </a>{" "}
+                </p>
+              </div>
+            </Form>
+          </Formik>
+        </div>
+        <div className={styles.container_for_image}>
+          <img src={LogIn} alt="Auth Image" fill />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Login;
