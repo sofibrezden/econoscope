09dbce86b63756690fd5157f5bc5f54ac45beb90
sofibrezden <sofibrezden9@gmail.com>
2024-12-15 00:57:31 +0200

    Updated db & enpoints logic

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from .routes import auth, main, history, prediction
 def create_app():
     app = Flask(__name__)
     app.secret_key = 'your_secret_key'
-    CORS(app, supports_credentials=True, origins=["http://localhost:3000"],allow_headers=["Content-Type", "Authorization"])
+    CORS(app, supports_credentials=True,resources={r"/*": {"origins": "*"}},allow_headers=["Content-Type", "Authorization"])
 
     app.register_blueprint(auth.bp)
     app.register_blueprint(main.bp)
