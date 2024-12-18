f683011aadfade6c63168838f557a239e74ae48f
sofibrezden <sofiaskymail@gmail.com>
2024-12-18 15:21:22 +0200

    Updated history page

--- frontend/src/pages/UserHistoryPage/UserHistoryPage.jsx
+++ frontend/src/pages/UserHistoryPage/UserHistoryPage.jsx
@@ -1,11 +1,13 @@
 import Header from "../HomePage/components/Header/Header";
 import ForecastSection from "./components/ForecastSection/ForecastSection";
+import Footer from "../HomePage/components/Footer/Footer";
 
 function UserHistoryPage (){
     return (
         <>
         <Header />
         <ForecastSection />
+        <Footer />
             </>
     )
 }
