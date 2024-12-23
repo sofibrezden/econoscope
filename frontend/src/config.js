cbded531370d09c1172f7340ca4f77ee5644ff06
sofiaprg <sofiaskymail@gmail.com>
2024-12-23 15:07:15 +0200

    updated urls

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-// src/config.js
-export const API_BASE_URL = "http://localhost:5000";
+export const API_BASE_URL = "http://localhost:5000/";
+export const API_Graphs_URL = "http://localhost:8050/";
