d29c53d704b3ae76cc8f70d6a98bdedeea623a0e
sofibrezden <sofibrezden9@gmail.com>
2024-12-23 15:04:44 +0200

    updated with urls from config.js

--- frontend/src/pages/UserHistoryPage/components/ForecastSection/ForecastSection.jsx
+++ frontend/src/pages/UserHistoryPage/components/ForecastSection/ForecastSection.jsx
@@ -1,10 +1,10 @@
 import React, {useCallback, useEffect, useState} from "react";
 import styles from "../../../UserHistoryPage/components/ForecastSection/ForecastSection.module.scss";
-
+import {API_BASE_URL} from "../../../../config";
 function ForecastSection() {
-    const [history, setHistory] = useState([]); // Дані нотаток
+    const [history, setHistory] = useState([]);
     const [error, setError] = useState(null);
-    const [currentPage, setCurrentPage] = useState(1); // Поточна сторінка
+    const [currentPage, setCurrentPage] = useState(1);
     const notesPerPage = 10;
 
     const getData = useCallback(async () => {
@@ -12,27 +12,30 @@ function ForecastSection() {
             const token = localStorage.getItem("authToken");
             if (!token) throw new Error("No authentication token found.");
 
-            const response = await fetch("http://127.0.0.1:5000/api/user-history", {
+            const response = await fetch(`${API_BASE_URL}/api/user-history`, {
                 method: "GET",
-                headers: {Authorization: `Bearer ${token}`},
+                headers: {'ngrok-skip-browser-warning': 'true',Authorization: `Bearer ${token}`},
             });
 
             if (!response.ok) throw new Error("Failed to fetch user history.");
 
             const data = await response.json();
             setHistory(data);
+            console.log(data)
         } catch (error) {
             console.error("Error fetching user history:", error);
             setError("User not authenticated or failed to fetch data.");
         }
     }, []);
+
     const deletePrediction = async (predictionId) => {
         try {
             const token = localStorage.getItem("authToken");
-            const response = await fetch("http://127.0.0.1:5000/delete-prediction", {
+            const response = await fetch(`${API_BASE_URL}/delete-prediction`, {
                 method: "DELETE",
                 headers: {
                     Authorization: `Bearer ${token}`,
+                    'ngrok-skip-browser-warning': 'true',
                     "Content-Type": "application/json",
                 },
                 body: JSON.stringify({prediction_id: predictionId}),
@@ -80,32 +83,46 @@ function ForecastSection() {
                     <th>Year</th>
                     <th>Result</th>
                     <th>Status</th>
+                    {/*<th>Status (comp. to 2023)</th>*/}
                     <th>Action</th>
                 </tr>
                 </thead>
                 {currentNotes.map((item) => (
                     <tr key={item.id}>
-                      <td className={styles.firstColumn}>{item.country}</td>
-                      <td>{item.age}</td>
-                      <td>{item.sex}</td>
-                      <td>{item.year}</td>
-                      <td>{(Number(item.prediction) * 100).toFixed(2)}%</td>
-                      <td className={styles["stability"]}>{item.state || "State..."}</td>
-                      <button
-                          className={styles.bin}
-                          onClick={() => deletePrediction(item.id)}
-                      >
-                        🗑
-                      </button>
+                        <td className={styles.firstColumn}>{item.country}</td>
+                        <td>{item.age}</td>
+                        <td>{item.sex}</td>
+                        <td>{item.year}</td>
+                        <td>{(Number(item.prediction) * 100).toFixed(2)}%</td>
+                        <td className={(() => {
+                            switch (item.state) {
+                                case "Decline":
+                                    return styles.decrease;
+                                case "Growth":
+                                    return styles.increase;
+                                default:
+                                    return "";
+                            }
+                        })()}>
+                            {item.state}
+                        </td>
+
+
+                        <button
+                            className={styles.bin}
+                            onClick={() => deletePrediction(item.id)}
+                        >
+                            🗑
+                        </button>
                     </tr>
                 ))}
             </table>
 
-          {/* Pagination */}
-          {totalPages > 1 && (
-              <div className={styles.pagination}>
-                <button onClick={prevPage} disabled={currentPage === 1} className={styles.pageButton}>
-                Prev
+            {/* Pagination */}
+            {totalPages > 1 && (
+                <div className={styles.pagination}>
+                    <button onClick={prevPage} disabled={currentPage === 1} className={styles.pageButton}>
+                        Prev
                     </button>
                     <span className={styles.pageNumber}>
                         Page {currentPage} of {totalPages}
