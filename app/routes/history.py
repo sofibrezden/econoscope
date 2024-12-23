7f7e7b9f2366f059a0f57ee950f90b2fcfe1e1e0
sofiaprg <sofiaskymail@gmail.com>
2024-12-23 14:47:08 +0200

    modified history endpoint with state

--- app/routes/history.py
+++ app/routes/history.py
@@ -1,22 +1,27 @@
 from flask import Blueprint, jsonify, request
 import sqlite3
 from contextlib import contextmanager
+from flask_cors import CORS
 from jwt import decode, ExpiredSignatureError, InvalidTokenError
 
 from app.models import get_user_predictions
 
 bp = Blueprint('history', __name__)
+CORS(bp, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+
 SECRET_KEY = "your_secret_key"
+
 
 @contextmanager
 def get_db_connection():
     conn = sqlite3.connect('users.db')
     conn.row_factory = sqlite3.Row
-    conn.execute("PRAGMA foreign_keys = ON") 
+    conn.execute("PRAGMA foreign_keys = ON")
     try:
         yield conn
     finally:
         conn.close()
+
 
 def get_user_id_from_token():
     token = request.headers.get("Authorization")
@@ -29,6 +34,7 @@ def get_user_id_from_token():
     except (ExpiredSignatureError, InvalidTokenError):
         return None
 
+
 @bp.route('/api/user-history', methods=['GET'])
 def get_user_history():
     user_id = get_user_id_from_token()
@@ -36,6 +42,7 @@ def get_user_history():
         return jsonify({"error": "User not authenticated"}), 401
 
     predictions = get_user_predictions(user_id)
+    print(predictions)
     history = [
         {
             "id": row[0],
@@ -44,6 +51,7 @@ def get_user_history():
             "sex": row[3],
             "year": row[4],
             "prediction": row[5],
+            "state": row[6],
         }
         for row in predictions
     ]
