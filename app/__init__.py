a04f31f34a1e3ce661ccf668729804389267977b
sofibrezden <sofiabrezden9@gmail.com>
2024-12-27 19:58:31 +0200

    updated db connection

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ def create_app():
     app = Flask(__name__)
     app.secret_key = 'your_secret_key'
     CORS(app, supports_credentials=True,resources={r"/*": {"origins": "*"}},allow_headers=["Content-Type", "Authorization"])
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://econoscope_user:y6ypDzBmGKmSFhQ41GJmCRftjNDWocLV@dpg-ctm1gojv2p9s738io6i0-a/econoscope'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://econoscope_user:y6ypDzBmGKmSFhQ41GJmCRftjNDWocLV@dpg-ctm1gojv2p9s738io6i0-a.oregon-postgres.render.com/econoscope'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     init_db(app)
     app.register_blueprint(auth.bp)
