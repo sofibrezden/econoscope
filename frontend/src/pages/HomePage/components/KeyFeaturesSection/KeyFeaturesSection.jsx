ae0c4fe8ac2f3d119a7deed60bcd32f3d6e40884
sofibrezden <sofiabrezden9@gmail.com>
2024-12-28 14:08:07 +0200

    added id to component

--- frontend/src/pages/HomePage/components/KeyFeaturesSection/KeyFeaturesSection.jsx
+++ frontend/src/pages/HomePage/components/KeyFeaturesSection/KeyFeaturesSection.jsx
@@ -4,32 +4,32 @@ import { cards } from "./constants";
 
 function HeroSection() {
   return (
-    <div>
-      <h1 className={styles.sectionTitle}>Key Features</h1>
-      <div className={styles.sectionContainer}>
-        <div>
-          <p className={styles.sectionSubTitle}>
-            Our model leverages state-of-the-art machine learning techniques to
-            forecast the US unemployment rate. It uses a combination of
-            macroeconomic data, labor market indicators, and internet search
-            data to predict future unemployment rates.
-          </p>
-          <button className={styles.viewDocumentation}>
-            View Documentation
-          </button>
-        </div>
-        <img src={ManCoins} alt="Man" className={styles.sectionImage} />
-      </div>
-      <div className={styles.cardContainer}>
-        {cards.map((item, index) => (
-          <div className={styles.wrapper} key={index}>
-            <img src={item.icon} alt="Icon" width={20} height={20} />
-            <h3 className={styles.cardTitle}>{item.title}</h3>
-            <p className={styles.cardDescription}>{item.description}</p>
+      <div id="keyFeature">
+          <h1 className={styles.sectionTitle}>Key Features</h1>
+          <div className={styles.sectionContainer}>
+              <div>
+                  <p className={styles.sectionSubTitle}>
+                      Our model leverages state-of-the-art machine learning techniques to
+                      forecast the US unemployment rate. It uses a combination of
+                      macroeconomic data, labor market indicators, and internet search
+                      data to predict future unemployment rates.
+                  </p>
+                  <button className={styles.viewDocumentation}>
+                      View Documentation
+                  </button>
+              </div>
+              <img src={ManCoins} alt="Man" className={styles.sectionImage}/>
           </div>
-        ))}
+          <div className={styles.cardContainer}>
+              {cards.map((item, index) => (
+                  <div className={styles.wrapper} key={index}>
+                      <img src={item.icon} alt="Icon" width={20} height={20}/>
+                      <h3 className={styles.cardTitle}>{item.title}</h3>
+                      <p className={styles.cardDescription}>{item.description}</p>
+                  </div>
+              ))}
+          </div>
       </div>
-    </div>
   );
 }
 
