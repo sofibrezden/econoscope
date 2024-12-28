0358aeacdf1f65dc0ea47b59cf953e1fa4cda8d7
sofibrezden <sofiabrezden9@gmail.com>
2024-12-28 14:07:48 +0200

    updated urls for buttons

--- frontend/src/pages/HomePage/components/HeroSection/HeroSection.jsx
+++ frontend/src/pages/HomePage/components/HeroSection/HeroSection.jsx
@@ -1,24 +1,42 @@
 import styles from "./HeroSection.module.scss";
 import CoinsLeaves from "../../../../assets/coins_leaves.png";
+import {useNavigate} from 'react-router-dom';
 
 function HeroSection() {
-  return (
-    <div className={styles.sectionContainer}>
-      <div>
-        <h1 className={styles.sectionTitle}>Stay Ahead In Employment Trends</h1>
-        <p className={styles.sectionSubTitle}>
-          Plan for the future with comprehensive data on global job markets and
-          unemployment rates. Gain valuable insights into economic shifts and
-          workforce trends to make better decisions in a competitive landscape.
-        </p>
-        <div className={styles.buttonsContainer}>
-          <button className={styles.getStarted}>Get Started</button>
-          <button className={styles.learnMore}>Learn More</button>
+    const navigate = useNavigate();
+
+    const handleGetStartedClick = () => {
+        navigate('/predict');
+    }
+
+    const handleLearnMoreClick = () => {
+        const section = document.getElementById('keyFeature');
+        if (section) {
+            section.scrollIntoView({behavior: 'smooth'});
+        }
+    }
+
+    return (
+        <div className={styles.sectionContainer}>
+            <div>
+                <h1 className={styles.sectionTitle}>Stay Ahead In Employment Trends</h1>
+                <p className={styles.sectionSubTitle}>
+                    Plan for the future with comprehensive data on global job markets and
+                    unemployment rates. Gain valuable insights into economic shifts and
+                    workforce trends to make better decisions in a competitive landscape.
+                </p>
+                <div className={styles.buttonsContainer}>
+                    <button className={styles.getStarted} onClick={handleGetStartedClick}>
+                        Get Started
+                    </button>
+                    <button className={styles.learnMore} onClick={handleLearnMoreClick}>
+                        Learn More
+                    </button>
+                </div>
+            </div>
+            <img src={CoinsLeaves} alt="Coins" className={styles.heroSectionImage}/>
         </div>
-      </div>
-      <img src={CoinsLeaves} alt="Coins" className={styles.heroSectionImage} />
-    </div>
-  );
+    );
 }
 
 export default HeroSection;
